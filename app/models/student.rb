2291f5322c23670632bcd7baa5e94a5c1ede78b3
NoNonsense126 <retail_nns@hotmail.com>
2014-10-16 23:02:40 +0800

    Updated Advanced Validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,25 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+	validate :validate_phone_number
+	email_validation = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z][a-z]+\z/i
+	validates :email,
+						:format => { :with => email_validation, :message => "Enter a valid Email address!"},
+					  :uniqueness => {:case_sensitive => false, :message => "Email already exists!"}
+	validates_numericality_of :age, :greater_than => 5
+
+	def name
+		"#{first_name} #{last_name}"
+	end
+
+	def age
+		Time.now.year - birthday.year
+	end
+
+	def validate_phone_number
+		unless phone.downcase.gsub(/[a-z]/, "").length >= 10
+			errors.add(:phone, "Need a valid phone number")
+		end
+	end
+
 end
